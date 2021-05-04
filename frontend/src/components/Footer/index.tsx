47d63a24e3974d3c361158c45f658215728f59cc
DaianeM <daianersm2@gmail.com>
2021-05-04 04:05:56 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/DaianeM"        target="_blank" rel="noreferrer">Daiane Matos</a></p>
+
+                <p className="text-light"><small>
+                LinkedIn: <a href="https://www.linkedin.com/in/daiane-matos-9963201a5/" target="_blank" rel="noreferrer">Daiane Matos</a></small></p>
+               
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
