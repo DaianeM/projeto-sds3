57909fd6e156334288df76d743ce82f5df52a85c
DaianeM <daianersm2@gmail.com>
2021-05-07 18:25:50 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -28,7 +28,7 @@ const DonutChart = () => {
     //  series: [477138, 499928, 444867, 220426, 473088],
     //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     // }
-
+    
     const options = {
         legend: {
             show: true
